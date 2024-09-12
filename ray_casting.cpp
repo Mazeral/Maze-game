b9515a4ec05dab5c96dbb19433105395d86fbab2
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-09-12 18:53:32 +0200

    Completed the ray casting.

--- ray_casting.cpp
+++ ray_casting.cpp
@@ -1,30 +1,38 @@
 #include "main.h"
 
-void rayCasting(SDL_Renderer* renderer, SDL_Rect player, int mazeWidth, int mazeHeight, int mazeCellSize) {
-    float rayAngle = 0.0f;
-    float rayDistance = 0.0f;
+void rayCasting(SDL_Renderer* renderer, SDL_Rect player, int mazeWidth, int mazeHeight, int mazeCellSize)
+{
+	float rayAngle = -30.0f * 3.14159f / 180.0f; // Start at -30 degrees
+	float rayDistance = 0.0f;
+	float rayX, rayY;
+	int wallX, wallY;
 
-    for (int i = 0; i < 360; i++) {
-        rayAngle = (float)i * 3.14159f / 180.0f;
-        rayDistance = 0.0f;
+	for (int i = 0; i < 60; i++)
+	{ /* Cast 60 rays, each 1 degree apart */
+		rayDistance = 0.0f;
 
-        while (rayDistance < 100.0f) {
-            float rayX = player.x + cos(rayAngle) * rayDistance;
-            float rayY = player.y + sin(rayAngle) * rayDistance;
+	while (rayDistance < 100.0f)
+	{
+		rayX = player.x + cos(rayAngle) * rayDistance;
+		rayY = player.y + sin(rayAngle) * rayDistance;
 
-            int wallX = (int)rayX / mazeCellSize;
-            int wallY = (int)rayY / mazeCellSize;
+		wallX = (int)rayX / mazeCellSize;
+		wallY = (int)rayY / mazeCellSize;
 
-            if (wallX >= 0 && wallX < mazeWidth && wallY >= 0 && wallY < mazeHeight) {
-                if (isWall(wallX, wallY)) {
-                    break;
-                }
-            }
+		if (wallX >= 0 && wallX < mazeWidth && wallY >= 0 && wallY < mazeHeight)
+		{
+			if (isWall(wallX, wallY))
+			{
+				break;
+			}
+		}
 
-            rayDistance += 1.0f;
-        }
+		rayDistance += 1.0f;
+	}
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderDrawLine(renderer, player.x, player.y, player.x + cos(rayAngle) * rayDistance, player.y + sin(rayAngle) * rayDistance);
-    }
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderDrawLine(renderer, player.x, player.y, player.x + cos(rayAngle) * rayDistance, player.y + sin(rayAngle) * rayDistance);
+
+	rayAngle += 1.0f * 3.14159f / 180.0f; // Increment angle by 1 degree
+	}
 }
