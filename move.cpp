1e9c8902f23ed70e1745074cf69406194b28a7a1
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-09-12 16:52:16 +0200

    fixed the collusion problem.

--- move.cpp
+++ move.cpp
@@ -1,42 +1,39 @@
 #include "main.h"
 
-void move(SDL_Rect* player, int playerSpeed)
+void handleMovement(SDL_Rect* player, int playerSpeed, const Uint8* keystates)
 {
-	/*Gets the input*/
-	const Uint8* keystates = SDL_GetKeyboardState(NULL);
-	/*These lines calculate the coordinates of the player within the maze. The mazeCellSize variable represents the size of each cell in the maze. By dividing the player's x and y coordinates by mazeCellSize, the code determines which cell in the maze the player is currently occupying.
-	 * This works because When you divide the player's position by the cell size, you're essentially converting the player's position from pixel coordinates to tile coordinates.*/
 	int mazeX = player->x / mazeCellSize;
 	int mazeY = player->y / mazeCellSize;
 
 	if (keystates[SDL_SCANCODE_UP])
 		player->y -= playerSpeed;
-	if (keystates[SDL_SCANCODE_DOWN]) 
+	if (keystates[SDL_SCANCODE_DOWN])
 		player->y += playerSpeed;
 	if (keystates[SDL_SCANCODE_LEFT])
 		player->x -= playerSpeed;
 	if (keystates[SDL_SCANCODE_RIGHT])
 		player->x += playerSpeed;
 
-	/* Check for collisions with the maze */ 
-	if (mazeY < mazeHeight && mazeX < mazeWidth && maze[mazeY][mazeX] == 1)
-	{
-		// If the player collides with a wall, move them back
-	// Before moving the player
+
+	if (mazeY < 0 || mazeX < 0 || mazeY >= mazeHeight || mazeX >= mazeWidth || maze[mazeY][mazeX] == 1) {
 	if (keystates[SDL_SCANCODE_UP])
-		if (mazeY - 1 >= 0 && maze[mazeY - 1][mazeX] != 1)
-			player->y -= playerSpeed;
-
+		player->y += playerSpeed;
 	if (keystates[SDL_SCANCODE_DOWN])
-		if (mazeY + 1 < mazeHeight && maze[mazeY + 1][mazeX] != 1)
-			player->y += playerSpeed;
-
+		player->y -= playerSpeed;
 	if (keystates[SDL_SCANCODE_LEFT])
-	    if (mazeX - 1 >= 0 && maze[mazeY][mazeX - 1] != 1)
-		player->x -= playerSpeed;
-
-	if (keystates[SDL_SCANCODE_RIGHT])
-	    if (mazeX + 1 < mazeWidth && maze[mazeY][mazeX + 1] != 1)
 		player->x += playerSpeed;
+	if (keystates[SDL_SCANCODE_RIGHT])
+		player->x -= playerSpeed;
 	}
+}
+
+bool isCollision(int mazeX, int mazeY)
+{
+	return (mazeY < mazeHeight && mazeX < mazeWidth && maze[mazeY][mazeX] == 1);
+}
+
+void move(SDL_Rect* player, int playerSpeed)
+{
+	const Uint8* keystates = SDL_GetKeyboardState(NULL);
+	handleMovement(player, playerSpeed, keystates);
 }
